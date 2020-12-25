2c0a0ad07892f6c4c83addc1403b363b1aeead0f
engruzairulhassan <engr.uzairulhassan@gmail.com>
2020-12-25 05:55:47 +0500

    code updated

--- histogram.py
+++ histogram.py
@@ -1,5 +1,12 @@
-list = [4, 6, 7]
-for i in list:
-    for a in range(i):
-        print('x', end="")
-    print('\n')
+def histogram(list):
+    for i in list:
+        for a in range(i):
+            print('x', end="")
+        print('\n')
+x = int(input('enter the number of list you want \n'))
+list = []
+for i in range(x):
+    u = int(input("enter number \n"))
+    list.append(u)
+print('list of numbers ', list)
+histogram(list)

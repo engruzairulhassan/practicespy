1e7e1e95336998f91e87ada67cfb788846cbaf0a
uzair <engr.uzairulhassan@gmail.com>
2020-12-30 07:05:19 +0500

    member

--- member.py
+++ member.py
@@ -4,21 +4,25 @@ def is_member(x,n):
             global b
             print(n, b)
             break
+    else:
+        print('not a member')
 
 selection = int(input("Enter a string or list of number\nFor String Enter 1 and for list Enter 2\n"))
 if selection == 1:
     x = input("Enter the string\n")
     n = input("Enter the which you want to check\n")
     b = " The letter is the member of the string"
+    is_member(x, n)
 elif selection == 2:
     x = []
     y = int(input("Enter the length of list you want to add\n"))
-    for i in range(y-1):
+    for i in range(y):
         num = input("Enter the number\n")
         x.append(num)
     n = input("Enter the which you want to check\n")
     b = " The number is member of the list"
+    is_member(x, n)
 else:
     print('invalid Selection')
-is_member(x,n)
+
 
